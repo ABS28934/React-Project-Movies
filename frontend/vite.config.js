05ca9b9a999bc2870a48d64b2e9fbd81e36a9f7b
ABS28934 <anandinambiar@gmail.com>
2025-10-21 18:07:25 +1100

    config redone

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
 
-// Convert import.meta.url to a file path
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
+// https://vite.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname),      
-  publicDir: path.resolve(__dirname, 'public'),
-  build: {
-    outDir: path.resolve(__dirname, '../dist'), 
-  },
   plugins: [react()],
 })
